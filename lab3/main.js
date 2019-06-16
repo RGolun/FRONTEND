dd49f440937f1f2528c5def959e7a864799143a0
RGolun <rgouski@gmail.com>
2019-06-16 11:12:24 +0200

    add module lab3

--- lab3/main.js
+++ lab3/main.js
@@ -33,7 +33,49 @@ Brzoza.prototype.iloscsloi = function () {
     console.log("Brzoza z liczbą słoi: " + this.sloje);
 };
 
+var Module = ( function () {
+    
+    var drzewa = [
+        new Drewno(200,900, true),
+        new Drewno(100,4000,true)
+    ];
+
+    var addDrewno = function (newDrewno) {
+        if (drzewa.find(function(drew) { return drew.sz == newDrewno.sz}))
+        {
+            console.log("ERROR! Drewno z podaną szerokością już istnieje");
+            return
+        }
+        drzewa.push(newDrewno);
+    };
+
+    return {
+
+        getDrewnoBySz: function(sz) {
+            return drzewa.find(function(drew) {return drew.sz == sz});
+        },
+
+        createDrew: function(sz,wys) {
+            var newDrew = new Drewno(sz,wys);
+            addDrewno(newDrew);
+        },
+
+        updateDrew: function(sz, newDrew) {
+            var drewIndex = drzewa.findIndex(function(drew) { return drew.sz == sz});
+            if (drewIndex == -1)
+            {
+                console.log("ERROR!");
+                return;
+            }
+            drzewa[drewIndex] = newDrew;
+        }
+    }
+})();
+
+
 var brzoza = new Brzoza(200, 2000, true);
+var drewkko = Module.getDrewnoBySz(100);
+console.log(drewkko);
 console.log("Szerokość drewna przed obcięciem: " + brzoza.sz);
 brzoza.obciecieDrewna(100,200);
 console.log("Drewno po obcieciu: " + brzoza.sz);
